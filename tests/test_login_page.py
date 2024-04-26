e9bd7a24459e0e1478f9a8fa9c0cad99283bf3f4
Oleg Kim <118339122+K2lighter@users.noreply.github.com>
2024-04-26 12:04:17 +0300

    Update test_login_page.py

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -18,7 +18,7 @@ def fibonacci(n):
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
-@pytest.mark.skip
+
 def test_select_product():
     options = Options()
     # options.add_argument("--headless")
